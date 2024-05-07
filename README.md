b0153127d8da36bda163b44fb07d5330ffbe1c2b
Eike Schaubeck <eike.schaubeck@tutanota.com>
2024-05-07 13:01:43 +0200

    added ranking to pretty print

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-# Kicktipp_Analyser
+# Kicktipp Analyser
 Einfaches Skript um die Platzierungen einer Kicktipp-Runde mit anderen Punkteregeln zu betrachten
 
 ## Benutzung:
-Zum Starten einfach die Main-Methode aufrufen, und die Fragen beantworten.
+Zum Starten einfach die Main-Methode aufrufen und die Fragen beantworten.
 
 Anschließend wird die Tipprunde mit den neu festgelegten Punkteregeln berechnet und ausgegeben.
 
+
 TODO:   
-- Einzelne Spieltage mit der Main-Methode anzeigen lassen. 
+- Einzelne Spieltage mit der Main-Methode anzeigen lassen (wird vielleicht auch zu unübersichtlich)
 - Pretty-Print für einzelne Spieltage
 - Verbessertes Error-Handling
 - exe-Datei für einfache Benutzung unter Windows
--- Season.py
+++ Season.py
@@ -15,23 +15,28 @@ class Season:
                     player.matchday_points.append(md_player.get_total_matchday_points())
 
     def pretty_print(self):
-        output = "┌".ljust(16, "─") + "┬"
+        # top frame
+        output = "┌".ljust(5, "─")
+        output += "┬".ljust(16, "─") + "┬"
         for i in range(34):
             output += "────┬"
 
-        output += "─────┐\n│ Name          │"
+        # headings
+        output += "─────┐\n│POS │ Name          │"
         for i in range(1, 35):
             output += " " + str(i).rjust(2) + " │"
         output += " GES │"
 
-        output += "\n├".ljust(17, "─") + "┼"
+        output += "\n├".ljust(6, "─") + "┼".ljust(16, "─") + "┼"
         for i in range(34):
             output += "────┼"
         output += "─────┤\n│ "
+        counter = 1
         for player in self.players:
-            output += player.name.ljust(14) + "│ "
+            output += (str(counter).rjust(2)).ljust(3) + "│" + player.name.ljust(15) + "│ "
             for points in player.matchday_points:
                 output += str(points).rjust(2) + " │ "
 
             output += str(player.get_total_saison_points()) + " │\n│ "
+            counter += 1
         return output[:-2]
